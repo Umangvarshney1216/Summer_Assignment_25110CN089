2e8955a90f16e1765ee94abd022a6294e4811c60
umang varshney <umangv9090@gmail.com>
2026-06-24 13:49:53 +0530

    DAY-24 assignment is uploaded

---
+++ DAY-24/q95.cpp
@@ -0,0 +1,20 @@
+//program to find the longest word
+
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+int main() {
+    string sentence, word, longestWord;
+    cout << "Enter a sentence: ";
+    getline(cin, sentence);
+    stringstream ss(sentence);
+    while (ss >> word) {
+        if (word.length() > longestWord.length()) {
+            longestWord = word;
+        }
+    }
+    cout << "Longest word: " << longestWord << endl;
+    cout << "Length: " << longestWord.length();
+    return 0;
+}
